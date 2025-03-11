6c14c3e2377e909b583bdc0e062947c867d8f117
mansan <65330511+minhas0@users.noreply.github.com>
2025-03-11 23:53:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -131,4 +131,5 @@ def start():
     return "Attendance Recorded and Emailed Successfully ✅"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
+
